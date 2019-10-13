c8f641367e1aa36385e1fb262406476001b20ee1
masahino <masahino@gmail.com>
2019-10-14 08:56:21 +0900

    add method

--- src/mruby_termbox.c
+++ src/mruby_termbox.c
@@ -1,16 +1,13 @@
+#include <string.h>
 #include <mruby.h>
+#include <mruby/array.h>
 #include <mruby/class.h>
 #include <mruby/data.h>
 #include <mruby/string.h>
 #include <termbox.h>
 
-static void mrb_termbox_event_free(mrb_state *mrb, void *ptr) {
-/*  if (ptr != NULL) {
-    free(ptr);
-  }
-*/
-}
-const static struct mrb_data_type mrb_termbox_event_data_type = { "Termbox::Event", mrb_termbox_event_free };
+const static struct mrb_data_type mrb_termbox_event_data_type = { "Termbox::Event", mrb_free };
+const static struct mrb_data_type mrb_termbox_cell_data_type = { "Termbox::Cell", mrb_free};
 
 mrb_value
 mrb_termbox_init(mrb_state *mrb, mrb_value self)
@@ -137,6 +134,25 @@ mrb_value
 mrb_termbox_cell_buffer(mrb_state *mrb, mrb_value self)
 {
   mrb_raise(mrb, E_NOTIMP_ERROR, "not yet implemented");
+  return mrb_nil_value();
+}
+
+mrb_value
+mrb_termbox_set_cell_buffer(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell_buffer, *ptr, *cell;
+  mrb_int len, i;
+  mrb_value buf;
+  mrb_get_args(mrb, "A", &buf);
+
+  cell_buffer = tb_cell_buffer();
+  ptr = cell_buffer;
+  len = RARRAY_LEN(buf);
+  for (i = 0; i < len; i++) {
+    cell = (struct tb_cell *)DATA_PTR(mrb_ary_ref(mrb, buf, i));
+    memcpy(ptr, cell, sizeof(struct tb_cell));
+    ptr++;
+  }
   return mrb_nil_value();
 }
 
@@ -277,14 +293,94 @@ mrb_termbox_event_y(mrb_state *mrb, mrb_value self)
   return mrb_fixnum_value(event->y);
 }
 
+mrb_value
+mrb_termbox_cell_init(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell;
+  
+  cell = mrb_malloc(mrb, sizeof(struct tb_cell));
+  cell->ch = 0;
+  cell->fg = 0;
+  cell->bg = 0;
+  DATA_TYPE(self) = &mrb_termbox_cell_data_type;
+  DATA_PTR(self) = cell;
+
+  return self;
+}
+
+mrb_value
+mrb_termbox_cell_ch(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  mrb_value ch = mrb_fixnum_value(cell->ch);
+  return mrb_funcall(mrb, ch, "chr", 1, mrb_str_new_cstr(mrb, "UTF-8"));
+}
+
+mrb_value
+mrb_termbox_cell_set_ch(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  mrb_value str, ch;
+  mrb_get_args(mrb, "o", &str);
+
+  switch(mrb_type(str)) {
+    case MRB_TT_FIXNUM:
+    ch = str;
+    break;
+    case MRB_TT_STRING:
+    ch = mrb_funcall(mrb, str, "ord", 0);
+    break;
+    default:
+    mrb_raise(mrb, E_ARGUMENT_ERROR, "invalid parameter");
+  }
+  cell->ch = mrb_fixnum(ch);
+  return mrb_nil_value();
+}
+
+mrb_value
+mrb_termbox_cell_fg(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  return mrb_fixnum_value(cell->fg);
+}
+
+mrb_value
+mrb_termbox_cell_set_fg(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  mrb_int fg;
+  mrb_get_args(mrb, "i", &fg);
+  cell->fg = fg;
+  return mrb_nil_value();
+}
+
+mrb_value
+mrb_termbox_cell_bg(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  return mrb_fixnum_value(cell->bg);
+}
+
+mrb_value
+mrb_termbox_cell_set_bg(mrb_state *mrb, mrb_value self)
+{
+  struct tb_cell *cell = (struct tb_cell *)DATA_PTR(self);
+  mrb_int bg;
+  mrb_get_args(mrb, "i", &bg);
+  cell->bg = bg;
+  return mrb_nil_value();
+}
+
 void
 mrb_mruby_termbox_gem_init(mrb_state *mrb)
 {
-  struct RClass *termbox_module, *termbox_event;
+  struct RClass *termbox_module, *termbox_event, *termbox_cell;
 
   termbox_module = mrb_define_module(mrb, "Termbox");
   termbox_event = mrb_define_class_under(mrb, termbox_module, "Event", mrb->object_class);
   MRB_SET_INSTANCE_TT(termbox_event, MRB_TT_DATA);
+  termbox_cell = mrb_define_class_under(mrb, termbox_module, "Cell", mrb->object_class);
+  MRB_SET_INSTANCE_TT(termbox_cell, MRB_TT_DATA);
 
   mrb_define_const(mrb, termbox_module, "MOD_ALT", mrb_fixnum_value(TB_MOD_ALT));
   mrb_define_const(mrb, termbox_module, "MOD_MOTION", mrb_fixnum_value(TB_MOD_MOTION));
@@ -329,6 +425,7 @@ mrb_mruby_termbox_gem_init(mrb_state *mrb)
   mrb_define_class_method(mrb, termbox_module, "blit", mrb_termbox_blit, MRB_ARGS_REQ(5));
 
   mrb_define_class_method(mrb, termbox_module, "cell_buffer", mrb_termbox_cell_buffer, MRB_ARGS_NONE());
+  mrb_define_class_method(mrb, termbox_module, "set_cell_buffer", mrb_termbox_set_cell_buffer, MRB_ARGS_REQ(1));
 
   mrb_define_const(mrb, termbox_module, "INPUT_CURRENT", mrb_fixnum_value(TB_INPUT_CURRENT));
   mrb_define_const(mrb, termbox_module, "INPUT_ESC", mrb_fixnum_value(TB_INPUT_ESC));
@@ -370,80 +467,89 @@ mrb_mruby_termbox_gem_init(mrb_state *mrb)
   mrb_define_method(mrb, termbox_event, "x", mrb_termbox_event_x, MRB_ARGS_NONE());
   mrb_define_method(mrb, termbox_event, "y", mrb_termbox_event_y, MRB_ARGS_NONE());
 
-mrb_define_const(mrb, termbox_module, "KEY_F1", mrb_fixnum_value(TB_KEY_F1));
-mrb_define_const(mrb, termbox_module, "KEY_F2", mrb_fixnum_value(TB_KEY_F2));
-mrb_define_const(mrb, termbox_module, "KEY_F3", mrb_fixnum_value(TB_KEY_F3));
-mrb_define_const(mrb, termbox_module, "KEY_F4", mrb_fixnum_value(TB_KEY_F4));
-mrb_define_const(mrb, termbox_module, "KEY_F5", mrb_fixnum_value(TB_KEY_F5));
-mrb_define_const(mrb, termbox_module, "KEY_F6", mrb_fixnum_value(TB_KEY_F6));
-mrb_define_const(mrb, termbox_module, "KEY_F7", mrb_fixnum_value(TB_KEY_F7));
-mrb_define_const(mrb, termbox_module, "KEY_F8", mrb_fixnum_value(TB_KEY_F8));
-mrb_define_const(mrb, termbox_module, "KEY_F9", mrb_fixnum_value(TB_KEY_F9));
-mrb_define_const(mrb, termbox_module, "KEY_F10", mrb_fixnum_value(TB_KEY_F10));
-mrb_define_const(mrb, termbox_module, "KEY_F11", mrb_fixnum_value(TB_KEY_F11));
-mrb_define_const(mrb, termbox_module, "KEY_F12", mrb_fixnum_value(TB_KEY_F12));
-mrb_define_const(mrb, termbox_module, "KEY_INSERT", mrb_fixnum_value(TB_KEY_INSERT));
-mrb_define_const(mrb, termbox_module, "KEY_DELETE", mrb_fixnum_value(TB_KEY_DELETE));
-mrb_define_const(mrb, termbox_module, "KEY_HOME", mrb_fixnum_value(TB_KEY_HOME));
-mrb_define_const(mrb, termbox_module, "KEY_END", mrb_fixnum_value(TB_KEY_END));
-mrb_define_const(mrb, termbox_module, "KEY_PGUP", mrb_fixnum_value(TB_KEY_PGUP));
-mrb_define_const(mrb, termbox_module, "KEY_PGDN", mrb_fixnum_value(TB_KEY_PGDN));
-mrb_define_const(mrb, termbox_module, "KEY_ARROW_UP", mrb_fixnum_value(TB_KEY_ARROW_UP));
-mrb_define_const(mrb, termbox_module, "KEY_ARROW_DOWN", mrb_fixnum_value(TB_KEY_ARROW_DOWN));
-mrb_define_const(mrb, termbox_module, "KEY_ARROW_LEFT", mrb_fixnum_value(TB_KEY_ARROW_LEFT));
-mrb_define_const(mrb, termbox_module, "KEY_ARROW_RIGHT", mrb_fixnum_value(TB_KEY_ARROW_RIGHT));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_LEFT", mrb_fixnum_value(TB_KEY_MOUSE_LEFT));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_RIGHT", mrb_fixnum_value(TB_KEY_MOUSE_RIGHT));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_MIDDLE", mrb_fixnum_value(TB_KEY_MOUSE_MIDDLE));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_RELEASE", mrb_fixnum_value(TB_KEY_MOUSE_RELEASE));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_WHEEL_UP", mrb_fixnum_value(TB_KEY_MOUSE_WHEEL_UP));
-mrb_define_const(mrb, termbox_module, "KEY_MOUSE_WHEEL_DOWN", mrb_fixnum_value(TB_KEY_MOUSE_WHEEL_DOWN));
+  mrb_define_const(mrb, termbox_module, "KEY_F1", mrb_fixnum_value(TB_KEY_F1));
+  mrb_define_const(mrb, termbox_module, "KEY_F2", mrb_fixnum_value(TB_KEY_F2));
+  mrb_define_const(mrb, termbox_module, "KEY_F3", mrb_fixnum_value(TB_KEY_F3));
+  mrb_define_const(mrb, termbox_module, "KEY_F4", mrb_fixnum_value(TB_KEY_F4));
+  mrb_define_const(mrb, termbox_module, "KEY_F5", mrb_fixnum_value(TB_KEY_F5));
+  mrb_define_const(mrb, termbox_module, "KEY_F6", mrb_fixnum_value(TB_KEY_F6));
+  mrb_define_const(mrb, termbox_module, "KEY_F7", mrb_fixnum_value(TB_KEY_F7));
+  mrb_define_const(mrb, termbox_module, "KEY_F8", mrb_fixnum_value(TB_KEY_F8));
+  mrb_define_const(mrb, termbox_module, "KEY_F9", mrb_fixnum_value(TB_KEY_F9));
+  mrb_define_const(mrb, termbox_module, "KEY_F10", mrb_fixnum_value(TB_KEY_F10));
+  mrb_define_const(mrb, termbox_module, "KEY_F11", mrb_fixnum_value(TB_KEY_F11));
+  mrb_define_const(mrb, termbox_module, "KEY_F12", mrb_fixnum_value(TB_KEY_F12));
+  mrb_define_const(mrb, termbox_module, "KEY_INSERT", mrb_fixnum_value(TB_KEY_INSERT));
+  mrb_define_const(mrb, termbox_module, "KEY_DELETE", mrb_fixnum_value(TB_KEY_DELETE));
+  mrb_define_const(mrb, termbox_module, "KEY_HOME", mrb_fixnum_value(TB_KEY_HOME));
+  mrb_define_const(mrb, termbox_module, "KEY_END", mrb_fixnum_value(TB_KEY_END));
+  mrb_define_const(mrb, termbox_module, "KEY_PGUP", mrb_fixnum_value(TB_KEY_PGUP));
+  mrb_define_const(mrb, termbox_module, "KEY_PGDN", mrb_fixnum_value(TB_KEY_PGDN));
+  mrb_define_const(mrb, termbox_module, "KEY_ARROW_UP", mrb_fixnum_value(TB_KEY_ARROW_UP));
+  mrb_define_const(mrb, termbox_module, "KEY_ARROW_DOWN", mrb_fixnum_value(TB_KEY_ARROW_DOWN));
+  mrb_define_const(mrb, termbox_module, "KEY_ARROW_LEFT", mrb_fixnum_value(TB_KEY_ARROW_LEFT));
+  mrb_define_const(mrb, termbox_module, "KEY_ARROW_RIGHT", mrb_fixnum_value(TB_KEY_ARROW_RIGHT));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_LEFT", mrb_fixnum_value(TB_KEY_MOUSE_LEFT));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_RIGHT", mrb_fixnum_value(TB_KEY_MOUSE_RIGHT));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_MIDDLE", mrb_fixnum_value(TB_KEY_MOUSE_MIDDLE));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_RELEASE", mrb_fixnum_value(TB_KEY_MOUSE_RELEASE));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_WHEEL_UP", mrb_fixnum_value(TB_KEY_MOUSE_WHEEL_UP));
+  mrb_define_const(mrb, termbox_module, "KEY_MOUSE_WHEEL_DOWN", mrb_fixnum_value(TB_KEY_MOUSE_WHEEL_DOWN));
 
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_TILDE", mrb_fixnum_value(TB_KEY_CTRL_TILDE));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_2", mrb_fixnum_value(TB_KEY_CTRL_2));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_A", mrb_fixnum_value(TB_KEY_CTRL_A));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_B", mrb_fixnum_value(TB_KEY_CTRL_B));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_C", mrb_fixnum_value(TB_KEY_CTRL_C));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_D", mrb_fixnum_value(TB_KEY_CTRL_D));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_E", mrb_fixnum_value(TB_KEY_CTRL_E));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_F", mrb_fixnum_value(TB_KEY_CTRL_F));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_G", mrb_fixnum_value(TB_KEY_CTRL_G));
-mrb_define_const(mrb, termbox_module, "KEY_BACKSPACE", mrb_fixnum_value(TB_KEY_BACKSPACE));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_H", mrb_fixnum_value(TB_KEY_CTRL_H));
-mrb_define_const(mrb, termbox_module, "KEY_TAB", mrb_fixnum_value(TB_KEY_TAB));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_I", mrb_fixnum_value(TB_KEY_CTRL_I));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_J", mrb_fixnum_value(TB_KEY_CTRL_J));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_K", mrb_fixnum_value(TB_KEY_CTRL_K));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_L", mrb_fixnum_value(TB_KEY_CTRL_L));
-mrb_define_const(mrb, termbox_module, "KEY_ENTER", mrb_fixnum_value(TB_KEY_ENTER));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_M", mrb_fixnum_value(TB_KEY_CTRL_M));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_N", mrb_fixnum_value(TB_KEY_CTRL_N));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_O", mrb_fixnum_value(TB_KEY_CTRL_O));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_P", mrb_fixnum_value(TB_KEY_CTRL_P));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_Q", mrb_fixnum_value(TB_KEY_CTRL_Q));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_R", mrb_fixnum_value(TB_KEY_CTRL_R));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_S", mrb_fixnum_value(TB_KEY_CTRL_S));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_T", mrb_fixnum_value(TB_KEY_CTRL_T));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_U", mrb_fixnum_value(TB_KEY_CTRL_U));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_V", mrb_fixnum_value(TB_KEY_CTRL_V));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_W", mrb_fixnum_value(TB_KEY_CTRL_W));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_X", mrb_fixnum_value(TB_KEY_CTRL_X));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_Y", mrb_fixnum_value(TB_KEY_CTRL_Y));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_Z", mrb_fixnum_value(TB_KEY_CTRL_Z));
-mrb_define_const(mrb, termbox_module, "KEY_ESC", mrb_fixnum_value(TB_KEY_ESC));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_LSQ_BRACKET", mrb_fixnum_value(TB_KEY_CTRL_LSQ_BRACKET));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_3", mrb_fixnum_value(TB_KEY_CTRL_3));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_4", mrb_fixnum_value(TB_KEY_CTRL_4));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_BACKSLASH", mrb_fixnum_value(TB_KEY_CTRL_BACKSLASH));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_5", mrb_fixnum_value(TB_KEY_CTRL_5));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_RSQ_BRACKET", mrb_fixnum_value(TB_KEY_CTRL_RSQ_BRACKET));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_6", mrb_fixnum_value(TB_KEY_CTRL_6));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_7", mrb_fixnum_value(TB_KEY_CTRL_7));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_SLASH", mrb_fixnum_value(TB_KEY_CTRL_SLASH));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_UNDERSCORE", mrb_fixnum_value(TB_KEY_CTRL_UNDERSCORE));
-mrb_define_const(mrb, termbox_module, "KEY_SPACE", mrb_fixnum_value(TB_KEY_SPACE));
-mrb_define_const(mrb, termbox_module, "KEY_BACKSPACE2", mrb_fixnum_value(TB_KEY_BACKSPACE2));
-mrb_define_const(mrb, termbox_module, "KEY_CTRL_8", mrb_fixnum_value(TB_KEY_CTRL_8));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_TILDE", mrb_fixnum_value(TB_KEY_CTRL_TILDE));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_2", mrb_fixnum_value(TB_KEY_CTRL_2));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_A", mrb_fixnum_value(TB_KEY_CTRL_A));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_B", mrb_fixnum_value(TB_KEY_CTRL_B));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_C", mrb_fixnum_value(TB_KEY_CTRL_C));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_D", mrb_fixnum_value(TB_KEY_CTRL_D));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_E", mrb_fixnum_value(TB_KEY_CTRL_E));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_F", mrb_fixnum_value(TB_KEY_CTRL_F));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_G", mrb_fixnum_value(TB_KEY_CTRL_G));
+  mrb_define_const(mrb, termbox_module, "KEY_BACKSPACE", mrb_fixnum_value(TB_KEY_BACKSPACE));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_H", mrb_fixnum_value(TB_KEY_CTRL_H));
+  mrb_define_const(mrb, termbox_module, "KEY_TAB", mrb_fixnum_value(TB_KEY_TAB));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_I", mrb_fixnum_value(TB_KEY_CTRL_I));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_J", mrb_fixnum_value(TB_KEY_CTRL_J));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_K", mrb_fixnum_value(TB_KEY_CTRL_K));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_L", mrb_fixnum_value(TB_KEY_CTRL_L));
+  mrb_define_const(mrb, termbox_module, "KEY_ENTER", mrb_fixnum_value(TB_KEY_ENTER));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_M", mrb_fixnum_value(TB_KEY_CTRL_M));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_N", mrb_fixnum_value(TB_KEY_CTRL_N));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_O", mrb_fixnum_value(TB_KEY_CTRL_O));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_P", mrb_fixnum_value(TB_KEY_CTRL_P));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_Q", mrb_fixnum_value(TB_KEY_CTRL_Q));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_R", mrb_fixnum_value(TB_KEY_CTRL_R));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_S", mrb_fixnum_value(TB_KEY_CTRL_S));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_T", mrb_fixnum_value(TB_KEY_CTRL_T));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_U", mrb_fixnum_value(TB_KEY_CTRL_U));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_V", mrb_fixnum_value(TB_KEY_CTRL_V));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_W", mrb_fixnum_value(TB_KEY_CTRL_W));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_X", mrb_fixnum_value(TB_KEY_CTRL_X));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_Y", mrb_fixnum_value(TB_KEY_CTRL_Y));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_Z", mrb_fixnum_value(TB_KEY_CTRL_Z));
+  mrb_define_const(mrb, termbox_module, "KEY_ESC", mrb_fixnum_value(TB_KEY_ESC));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_LSQ_BRACKET", mrb_fixnum_value(TB_KEY_CTRL_LSQ_BRACKET));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_3", mrb_fixnum_value(TB_KEY_CTRL_3));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_4", mrb_fixnum_value(TB_KEY_CTRL_4));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_BACKSLASH", mrb_fixnum_value(TB_KEY_CTRL_BACKSLASH));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_5", mrb_fixnum_value(TB_KEY_CTRL_5));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_RSQ_BRACKET", mrb_fixnum_value(TB_KEY_CTRL_RSQ_BRACKET));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_6", mrb_fixnum_value(TB_KEY_CTRL_6));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_7", mrb_fixnum_value(TB_KEY_CTRL_7));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_SLASH", mrb_fixnum_value(TB_KEY_CTRL_SLASH));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_UNDERSCORE", mrb_fixnum_value(TB_KEY_CTRL_UNDERSCORE));
+  mrb_define_const(mrb, termbox_module, "KEY_SPACE", mrb_fixnum_value(TB_KEY_SPACE));
+  mrb_define_const(mrb, termbox_module, "KEY_BACKSPACE2", mrb_fixnum_value(TB_KEY_BACKSPACE2));
+  mrb_define_const(mrb, termbox_module, "KEY_CTRL_8", mrb_fixnum_value(TB_KEY_CTRL_8));
+
+  mrb_define_method(mrb, termbox_cell, "initialize", mrb_termbox_cell_init, MRB_ARGS_NONE());
+  mrb_define_method(mrb, termbox_cell, "ch", mrb_termbox_cell_ch, MRB_ARGS_NONE());
+  mrb_define_method(mrb, termbox_cell, "fg", mrb_termbox_cell_fg, MRB_ARGS_NONE());
+  mrb_define_method(mrb, termbox_cell, "bg", mrb_termbox_cell_bg, MRB_ARGS_NONE());
+
+  mrb_define_method(mrb, termbox_cell, "ch=", mrb_termbox_cell_set_ch, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, termbox_cell, "fg=", mrb_termbox_cell_set_fg, MRB_ARGS_REQ(1));
+  mrb_define_method(mrb, termbox_cell, "bg=", mrb_termbox_cell_set_bg, MRB_ARGS_REQ(1));
 
 }
 
