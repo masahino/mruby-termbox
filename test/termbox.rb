28be5b3c9a772d083fb46fedc05036364bdd2dfb
masahino <masahino@gmail.com>
2020-08-04 16:13:37 +0900

    add test

--- test/termbox.rb
+++ test/termbox.rb
@@ -19,3 +19,28 @@ assert("constants") do
   assert_equal 3, Termbox::OUTPUT_216
   assert_equal 4, Termbox::OUTPUT_GRAYSCALE
 end
+
+assert('size') do
+  Termbox.init_file("test_term")
+  w = Termbox.width
+  h = Termbox.height
+  assert_equal [w, h], Termbox.size
+#  Termbox.shutdown
+end
+
+assert('Cell.new with args') do
+  cell = Termbox::Cell.new('a')
+  assert_equal 'a', cell.ch
+  assert_equal 0, cell.fg
+  assert_equal 0, cell.bg
+
+  cell = Termbox::Cell.new('b', 2)
+  assert_equal 'b', cell.ch
+  assert_equal 2, cell.fg
+  assert_equal 0, cell.bg
+
+  cell = Termbox::Cell.new('c', 2, 5)
+  assert_equal 'c', cell.ch
+  assert_equal 2, cell.fg
+  assert_equal 5, cell.bg
+end
