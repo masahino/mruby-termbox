2ccf8594c6b2ef9b26694d8d5df13832d2e1c9c1
masahino <masahino@gmail.com>
2020-08-04 16:12:08 +0900

    add MRB_UTF8_STRING

--- .travis_build_config.rb
+++ .travis_build_config.rb
@@ -1,5 +1,6 @@
 MRuby::Build.new do |conf|
   toolchain :gcc
+  conf.cc.defines = %w(MRB_UTF8_STRING)
   conf.gembox 'default'
   conf.gem File.expand_path(File.dirname(__FILE__)) do |g|
     g.build_termbox
